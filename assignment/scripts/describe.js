5da68417e3690cbc08098a88f01de8f1eaf8fcaf
Steve Savstrom <steve.savstrom@gmail.com>
2021-05-11 20:32:52 -0500

    Week 2 Assignment Commit

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,6 +1,6 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening.
 // They are in isolation -- variables in one question do not affect other questions.
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
@@ -26,10 +26,10 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We make a variable called name and set it to 'Dane' as a string.
+// Then we check if the name is an exact match to 'Mary'.
+// If it is an exact match to 'Mary' then we console.log 'Hi, Mary!'
+// If it is not an exact match to 'Mary' then we console.log 'How do you do?'
 
 //CODE
 /*
@@ -45,10 +45,11 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We declare a variable called secret and a variable called code and set it to 123 as number.
+// Then we check if the code is an exact match to 123.
+// If the code is an exact match to 123 then we declare the variable secret to be 'super' and then multiply the code by 2 which gives us 246.
+// Then we check if the result is greater than 250 which is false.
+// Then we console.log(secret) which gives us 'super'.
 
 //CODE
 /*
@@ -70,10 +71,10 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// We declare 3 variables: isStudent and set it to true, age is set to 34, and zip is set to 55407.
+// We check if isStudent is an exact match with true and zip is greater than 8000 which is false.
+// Then we check if isStudent is an exact match with false or age is less than 30 which is false.
+// Then we check if isStudent is an exact match with true which is true so we console.log('Welcome to Prime!')
 
 //CODE
 /*
@@ -97,7 +98,7 @@ if (isStudent === true && zip > 80000 ) {
 
 
 // REVERSE
-// Now, for questions 4-6, we'll give you a description and some code. 
+// Now, for questions 4-6, we'll give you a description and some code.
 // Some of the code will NOT match the description.
 // Comment above the problem code, and describe a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
@@ -106,16 +107,16 @@ if (isStudent === true && zip > 80000 ) {
 // 0. (DESCRIPTION OF CORRECT BEHAVIOR)
 // We make a variable called number and set it to 1 as a number.
 // Then we increment the number variable. Number is now 2.
-// We check if number is greater than or equal to 2. 
+// We check if number is greater than or equal to 2.
 // -- it is, so we console.log 'yes'
 
 //CODE
 /*
 let number = 1;
 
-// FIX - number-- decrements number, but the instructions ask to increment. 
+// FIX - number--; decrements number, but the instructions ask to increment.
 // Should be number++.
-number--; 
+number--;
 
 if (number >= 2) {
   console.log('yes');
@@ -125,45 +126,56 @@ if (number >= 2) {
 
 
 
-//4. We start with three variables -- colorOne is set to 'blue' and 
-// colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//4. We start with three variables -- colorOne is set to 'blue' and
+// colorTwo is set to 'red', and mix is set to true. We check if mix is true
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+// FIX - colorOne should be set to 'blue'
+// Should be let colorOne = 'blue'
 let colorOne = 'red';
+// FIX - colorTwo should be set to 'red'
+// Should be let colorTwo = 'red';
 let colorTwo = 'blue';
 let mix = true;
 
 if (mix === true) {
   colorOne = 'purple';
+// FIX - colorTwo should also be purple
+// Should add line for colorTwo = 'purple';
 }
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+// We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
 
 /*
 let temp = 40;
 const time = 4;
 
+// FIX - The description says 'and' so the conditional syntax should use &&
+// Should be if (temp > 39 && time >= 4) {
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
-// We check if age is greater than or equal to minAge 
+// We check if age is greater than or equal to minAge
 // -- it is, so we console.log 'enter'.
 
 /*
 let age = 21;
 const minAge = 21;
 
+// FIX - The description says 'greater than or equal to' so conditional syntax should use >=
+// Should be if(minAge >= age) {
 if(minAge <= age) {
+// FIX - age is greater than or equal to minAge so console.log should be 'enter'
+// Should be console.log('enter');
   console.log('no entry');
 } else {
   console.log('enter');
 }
 */
-
